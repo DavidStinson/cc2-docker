f9cad582b9c9ae56682e003b30e65bb83be65319
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-08-01 07:33:21 -0500

    finish part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cats/', views.cats_index, name='cats_index'),
+  path('cats/<int:cat_id>/', views.cats_detail, name='cats_detail'),
 ]
